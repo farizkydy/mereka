c9534e19c7c995097ecd3da716cd32a2048849f8
Fariz <fariz.rizaldy@salt.co.id>
2024-02-15 20:17:19 +0700

    module works

--- clean-architecture-angular/src/presentation/app/app.component.ts
+++ clean-architecture-angular/src/presentation/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetJsonContentVotingUsecase } from 'src/@core/usecase/usecase/voting/get-json-content-voting.usecase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'clean-architecture-angular';
+
+  constructor(
+    private getJsonContentVotingUsecase: GetJsonContentVotingUsecase
+  ) {}
+
+  ngOnInit(): void {
+    this.getJsonContent()
+  }
+
+  getJsonContent() {
+    this.getJsonContentVotingUsecase.execute().subscribe((res) => {
+      console.log('res', res);
+    })
+  }
 }
--- clean-architecture-angular/src/presentation/app/app.module.ts
+++ clean-architecture-angular/src/presentation/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from 'src/data/core.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VotingComponent } from './voting/voting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VotingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
